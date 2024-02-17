f97e0a8fe1f7ec9bbea4a92088e97dcb2a39d11c
Prabeshpd <prabesh1995nepalktm@gmail.com>
2024-02-17 17:50:52 +0545

    [#4] Add user signup api

---
+++ src/services/users/users.ts
@@ -0,0 +1,39 @@
+import * as crypt from '@/lib/crypt';
+import { UserPayload, UserSchema, IUserRepository } from '@/repositories/users';
+
+import UserError from './error';
+
+interface IUserManager {
+  create: (payload: UserPayload) => Promise<UserSchema>;
+}
+
+class UserManager implements IUserManager {
+  private userRepository: IUserRepository;
+
+  constructor(userRepository: IUserRepository) {
+    this.userRepository = userRepository;
+  }
+
+  create = async (payload: UserPayload) => {
+    if (!payload.email || !payload.password) {
+      throw new UserError({
+        message: 'Please enter email and password.'
+      });
+    }
+
+    const user = await this.userRepository.findByEmail(payload.email);
+    if (user) {
+      throw new UserError({ message: 'Account with this email already exists.' });
+    }
+
+    const encryptedPassword = await crypt.hash(payload.password);
+    const userPayload = {
+      ...payload,
+      password: encryptedPassword
+    };
+
+    return this.userRepository.createUser(userPayload);
+  };
+}
+
+export default UserManager;
