bd1e7041f5c6f1ec6690c4f91416c55ca753eb90
Prabeshpd <prabesh1995nepalktm@gmail.com>
2024-02-19 08:07:19 +0545

    [#31] Add structured logging using pino

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,8 +2,9 @@ import { StatusCodes } from 'http-status-codes';
 import { NextFunction, Request, Response } from 'express';
 
 import { appConfig } from '@/config/appConfig';
-import * as jwt from '@/lib/jwt';
 import ApiError from '@/lib/apiError';
+import * as jwt from '@/lib/jwt';
+import { childLogger } from '@/lib/logger';
 import { UserSchema } from '@/repositories/users';
 
 export interface AuthorizedRequest extends Request {
@@ -12,17 +13,24 @@ export interface AuthorizedRequest extends Request {
 
 async function authenticate(request: Request, response: Response, next: NextFunction) {
   const authorizedRequest = request as AuthorizedRequest;
+  const logger = childLogger({
+    moduleName: 'Authentication'
+  });
+
   try {
+    logger.info('Authentication process started');
     const { accessTokenSecret } = appConfig.auth;
 
     const secret = `${accessTokenSecret}`;
     const token = request.headers.authorization;
 
     if (!token) {
+      logger.error({ error: { message: 'No authorization header set' } });
       return response.status(StatusCodes.UNAUTHORIZED).json({ message: 'No authorization header set' });
     }
 
     if (!token.includes('Bearer')) {
+      logger.error({ error: { message: "Authorization header doesn't include a Bearer token" } });
       return response
         .status(StatusCodes.UNAUTHORIZED)
         .json({ message: "Authorization header doesn't include a Bearer token" });
@@ -33,13 +41,16 @@ async function authenticate(request: Request, response: Response, next: NextFunc
     try {
       const decodedResult = (await jwt.verifyToken(bearerToken, secret)) as UserSchema;
       authorizedRequest.user = decodedResult;
+      logger.info('Authentication completed');
 
       next();
     } catch (err) {
+      logger.error({ error: { message: 'Invalid Token' } });
       return response.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid Token' });
     }
   } catch (err) {
     const error = new ApiError({ message: 'Something went wrong', code: StatusCodes.INTERNAL_SERVER_ERROR });
+    logger.error({ error: err });
 
     return next(error);
   }
--- src/routes/rootRouter.ts
+++ src/routes/rootRouter.ts
@@ -1,6 +1,5 @@
 import { Request, Response, Router } from 'express';
 
-// import PetControllers from '@/controllers/pets';
 import userRouter from './users/users';
 import authRouter from './auth/auth';
 import petRouter from './pets/pets';
@@ -12,7 +11,6 @@ generalRouter.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-// generalRouter.get('/pets', PetControllers.listPets);
 appRouter.use('/users', userRouter);
 appRouter.use('/auth', authRouter);
 appRouter.use('/pets', petRouter);
